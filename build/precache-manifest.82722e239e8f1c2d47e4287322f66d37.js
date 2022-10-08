f4e5a5ada559b383f6368d920166f8e4e42a47ad
shumil573 <heshuyao@bupt.edu.cn>
2022-10-08 08:46:17 +0800

    添加ghpages build文件

---
+++ build/precache-manifest.82722e239e8f1c2d47e4287322f66d37.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "34514eb7cee1288c2f3b",
+    "url": "/re-video-demo/static/css/main.10f9752a.chunk.css"
+  },
+  {
+    "revision": "34514eb7cee1288c2f3b",
+    "url": "/re-video-demo/static/js/main.34514eb7.chunk.js"
+  },
+  {
+    "revision": "a4e601787b5e71605368",
+    "url": "/re-video-demo/static/js/1.a4e60178.chunk.js"
+  },
+  {
+    "revision": "91cd332430ab46d200d4",
+    "url": "/re-video-demo/static/css/2.39870e82.chunk.css"
+  },
+  {
+    "revision": "91cd332430ab46d200d4",
+    "url": "/re-video-demo/static/js/2.91cd3324.chunk.js"
+  },
+  {
+    "revision": "575e6dab07523ac3938c",
+    "url": "/re-video-demo/static/js/runtime~main.575e6dab.js"
+  },
+  {
+    "revision": "3a74bdfd06529bc27f09d9ad0b831284",
+    "url": "/re-video-demo/static/media/logo.3a74bdfd.jpg"
+  },
+  {
+    "revision": "5aebcfb18b663f93906f796a373eaf47",
+    "url": "/re-video-demo/static/media/image1-00001.5aebcfb1.jpeg"
+  },
+  {
+    "revision": "ab35f9c1c5ec5e2fe5a5f7e9d09fe61a",
+    "url": "/re-video-demo/static/media/image2-00001.ab35f9c1.jpeg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/re-video-demo/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "03a8033e5f302a8c1a61b239f498950c",
+    "url": "/re-video-demo/index.html"
+  }
+];
